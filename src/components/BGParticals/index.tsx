d6cdacaa77765b0feb7e315d09c1f2161e7514d2
amstry <vanva44@gmail.com>
2024-03-02 18:26:36 +0700

    init + banner + bgparticals + globalstyles

---
+++ src/components/BGParticals/index.tsx
@@ -0,0 +1,87 @@
+import { useCallback } from 'react';
+import Particles from 'react-tsparticles';
+import type { Engine } from 'tsparticles-engine';
+import { loadSlim } from 'tsparticles-slim'; 
+
+const BGParticles = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: 'push'
+            },
+            onHover: {
+              enable: false,
+              mode: 'repulse'
+            },
+            resize: true
+          },
+          modes: {
+            push: {
+              quantity: 4
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4
+            }
+          }
+        },
+        particles: {
+          color: {
+            value: 'rgba(134, 255, 75, 1)'
+          },
+          links: {
+            color: 'rgba(134, 255, 75, 1)',
+            distance: 150,
+            enable: true,
+            opacity: 0.2,
+            width: 1
+          },
+          move: {
+            enable: true,
+            speed: 2,
+            direction: 'none',
+            random: false,
+            straight: false,
+            out_mode: 'out',
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200
+            }
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800
+            },
+            value: 80
+          },
+          opacity: {
+            value: 0.5
+          },
+          shape: {
+            type: 'circle'
+          },
+          size: {
+            value: { min: 1, max: 5 }
+          }
+        },
+        detectRetina: true
+      }}
+    />
+  );
+};
+
+export default BGParticles;
---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,21 @@
+import BannerMain from '../../assets/imgs/banner.png';
+import Banner2x from '../../assets/imgs/banner-2x.png';
+import BannerSm from '../../assets/imgs/banner-sm.png';
+import BannerSm2x from '../../assets/imgs/banner-sm-2x.png';
+import styles from './index.module.scss';
+
+const Banner = () => (
+  <picture>
+    <source media="(min-width: 800px)" srcSet={`${BannerMain} 1x, ${Banner2x} 2x`} />
+    <source media="(min-width: 400px)" srcSet={`${BannerSm} 1x, ${BannerSm2x} 2x`} />
+    <img
+      className={styles.banner}
+      src={BannerMain}
+      alt="Rick and Morty logo"
+      width={800}
+      height={251}
+    />
+  </picture>
+);
+
+export default Banner;
